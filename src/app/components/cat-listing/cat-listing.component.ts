26016fbcaadc936f9fb0f92aac5628346fbda47d
syed wakil <syedawakil@gmail.com>
2020-06-12 10:09:32 +1000

    created common function and stub json for test

--- src/app/components/cat-listing/cat-listing.component.ts
+++ src/app/components/cat-listing/cat-listing.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CatApiService } from '../../services/cat-api.service';
-import * as _ from 'lodash';
+import forEach  from 'lodash/forEach';
+import * as stubObj from "../cat-listing/stub-data.json";
+import { orderList } from './../../shared/common-functions';
+
 
 @Component({
   selector: 'app-cat-listing',
@@ -11,161 +14,108 @@ export class CatListingComponent implements OnInit {
 
   originalList: any = [];
   sortedList: any = [];
+  errorMessage = "";
+  errorStatus: boolean = false;
 
-  stubList = [
-    {
-        "name": "Bob",
-        "gender": "Male",
-        "age": 23,
-        "pets": [
-            {
-                "name": "Garfield",
-                "type": "Cat"
-            },
-            {
-                "name": "Fido",
-                "type": "Dog"
-            }
-        ]
-    },
-    {
-        "name": "Jennifer",
-        "gender": "Female",
-        "age": 18,
-        "pets": [
-            {
-                "name": "Garfield",
-                "type": "Cat"
-            }
-        ]
-    },
-    {
-        "name": "Steve",
-        "gender": "Male",
-        "age": 45,
-        "pets": null
-    },
-    {
-        "name": "Fred",
-        "gender": "Male",
-        "age": 40,
-        "pets": [
-            {
-                "name": "Tom",
-                "type": "Cat"
-            },
-            {
-                "name": "Max",
-                "type": "Cat"
-            },
-            {
-                "name": "Sam",
-                "type": "Dog"
-            },
-            {
-                "name": "Jim",
-                "type": "Cat"
-            }
-        ]
-    },
-    {
-        "name": "Samantha",
-        "gender": "Female",
-        "age": 40,
-        "pets": [
-            {
-                "name": "Tabby",
-                "type": "Cat"
-            }
-        ]
-    },
-    {
-        "name": "Alice",
-        "gender": "Female",
-        "age": 64,
-        "pets": [
-            {
-                "name": "Simba",
-                "type": "Cat"
-            },
-            {
-                "name": "Nemo",
-                "type": "Fish"
-            }
-        ]
-    }
-];
+
+  jsonDataStub: any = (stubObj as any).default; // this is for test
   constructor(private catServie: CatApiService) { }
 
   ngOnInit(): void {
     this.getCatList();
-    //this.normaliseList(this.originalList);
   }
 
   getCatList() {
-    this.originalList= this.stubList;
-    this.normaliseList(this.originalList);
-    // this.catServie.getCatListing().subscribe(
-    //   (resp) => {
-    //     console.log(resp);
-    //     this.originalList = resp;
-    //     console.log('this.ori', this.originalList);
-    //     this.normaliseList(this.originalList);
-    //   }
-    //   //add error
+    // this.originalList= this.stubList;
+    // this.normaliseList(this.originalList);
+    this.catServie.getCatListing().subscribe(
+      (resp) => {
+        console.log('resp', resp);
+        this.originalList = resp;
+        console.log('this.ori', this.originalList);
 
-    // );
+        this.normaliseList(this.originalList);
+      },
+      err => {
+        console.log("http error", err);
+        this.errorStatus = true;
+        this.errorMessage = err.statusText;
+
+      }
+
+
+    );
   }
 
   normaliseList(ArrayList: any) {
     console.log('normalise', ArrayList);
-    let maleList:any = [], femaleList: any =[];
+    let maleList: any = [], femaleList: any = [];
     let maleArrayNames = [];
     let femaleArrayNames = [];
-     
+
 
     if (ArrayList && ArrayList.length > 0) {
       console.log('start');
-      _.forEach(ArrayList, record => {
-        let tempList:any = []; 
+      forEach(ArrayList, record => {
+        let tempList: any = [];
 
         console.log('name:', record.name);
         let catList = record.pets?.filter(
-          pet => pet.type === 'Cat');
+          pet => pet.type.toUpperCase() === 'CAT');
 
         console.log('catList', catList);
-        
-        if(record.gender && record.gender==='Male' ) {
-          if(catList!=null)
-          maleList.push(catList);
+
+        if (record.gender && record.gender.toUpperCase() === 'MALE') {
+          if (catList != null)
+            maleList.push(catList);
 
         } else {
-          if(catList!=null)
-          femaleList.push(catList);
+          if (catList != null)
+            femaleList.push(catList);
         }
       }
       );
 
     }
-    _.forEach(maleList, records =>{
-      _.forEach(records, item =>{
-        maleArrayNames.push({name:item.name});
+    forEach(maleList, records => {
+      forEach(records, item => {
+        maleArrayNames.push({ name: item.name });
       });
-      
+
     });
-    _.forEach(femaleList, records =>{
-      _.forEach(records, item =>{
-        femaleArrayNames.push({name:item.name});
+    forEach(femaleList, records => {
+      forEach(records, item => {
+        femaleArrayNames.push({ name: item.name });
       });
-      
+
     });
-    let _newObjMale = { Type: "Male", List: maleArrayNames };
-    let _newObjFemale = {Type: "Female", List: femaleArrayNames };
-     this.sortedList.push(_newObjMale);
-     this.sortedList.push(_newObjFemale);
-     
-     console.log("this.sortedList", this.sortedList);
+   
+    femaleArrayNames = orderList(femaleArrayNames);
+    maleArrayNames = orderList(maleArrayNames); 
+  
+
+   this.setSortedList(maleArrayNames,femaleArrayNames);
+
+   
 
   }
+
+  
+  setSortedList(maleList, femaleList) {
+    let _newObjMale = { Type: "Male", List: maleList };
+    let _newObjFemale = { Type: "Female", List: femaleList };
+    this.sortedList.push(_newObjMale);
+    this.sortedList.push(_newObjFemale);
+  }
+
+  RefreshData() {
+    this.errorStatus = false;
+    this.originalList = [];
+    this.sortedList = [];
+    this.getCatList();
+
+  }
+
+  
 
 }
---
+++ src/app/shared/common-functions.ts
@@ -0,0 +1,5 @@
+import  orderBy  from 'lodash/orderBy';
+
+export function orderList(list: any) {
+    return  orderBy(list, ['name'], ['asc']);  
+}
