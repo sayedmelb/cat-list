69175cdce4e35ec1a086274bd03ed1c8ed06d153
syed wakil <syedawakil@gmail.com>
2020-06-13 05:20:26 +1000

    added few unit test cases

--- src/app/components/cat-listing/cat-listing.component.spec.ts
+++ src/app/components/cat-listing/cat-listing.component.spec.ts
@@ -1,25 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CatListingComponent } from './cat-listing.component';
+import { CatApiService } from '../../services/cat-api.service';
+import {DebugElement} from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+
+let catService : CatApiService;
 
 describe('CatListingComponent', () => {
   let component: CatListingComponent;
   let fixture: ComponentFixture<CatListingComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
+
+    // Create jasmine spy object 
+    //catService = jasmine.createSpyObj('CatApiService', 'getCatListing');
+    // Provide the dummy/mock data to sortNumberData method.
+    //sortServiceSpy.getCatListing.returnValue([someRandomArray]);
+
     TestBed.configureTestingModule({
-      declarations: [ CatListingComponent ]
+      imports: [RouterTestingModule],
+      declarations: [ CatListingComponent ],
+      providers: [CatApiService, HttpClient, HttpHandler],
     })
     .compileComponents();
   }));
 
+  
+
   beforeEach(() => {
     fixture = TestBed.createComponent(CatListingComponent);
     component = fixture.componentInstance;
+    catService = TestBed.get(CatApiService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create Cat listing component', () => {
+    const fixture = TestBed.createComponent(CatListingComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.ngOnInit();
+
+    expect(app).toBeTruthy();
   });
 });
--- src/app/services/cat-api.service.spec.ts
+++ src/app/services/cat-api.service.spec.ts
@@ -1,16 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CatApiService } from './cat-api.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('CatApiService', () => {
   let service: CatApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [CatApiService]
+    });
     service = TestBed.inject(CatApiService);
   });
 
-  it('should be created', () => {
+  it('service api should be created', () => {
     expect(service).toBeTruthy();
   });
 });
